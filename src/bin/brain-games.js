04c8b676d9489a2653fc8c1dc8eb1bd92eb98d8f
Dementeey-home <metallist9656@gmail.com>
2018-03-05 23:31:59 +0200

    install eslin and corrected bugs

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { yourName, hello } from "../index";
+import { yourName, hello } from '../index';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 hello(yourName());
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import readlineSync from 'readline-sync';
-export const yourName = () => 
-  readlineSync.question('May I have your name? ');
-export const hello = name => console.log(`Hello, ${name}!`);
 
+export const yourName = () => readlineSync.question('May I have your name? ');
+export const hello = name => console.log(`Hello, ${name}!`);
