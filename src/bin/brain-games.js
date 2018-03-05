dd555a40b1521abe140e980f203806474dbbe0fe
Dementeey-home <metallist9656@gmail.com>
2018-03-05 21:57:21 +0200

    second chekpoint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import { yourName, hello } from "../index";
+
 console.log("Welcome to the Brain Games!");
+hello(yourName());
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+export const yourName = () => 
+  readlineSync.question('May I have your name? ');
+export const hello = name => console.log(`Hello, ${name}!`);
+
