7d65f51034c845ccae0fb297fd106c9693034e1c
Dementeey-home <metallist9656@gmail.com>
2018-03-11 11:51:36 +0200

    fix loop

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,16 +6,31 @@ const generator = () => () => {
   const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
   const quest = getRandomInt(2, 110);
 
-  const isPrime = (num) => {
-    for (let i = 2; i < num; i += 1) {
+  // const isPrime = (num) => {
+  //   for (let i = 2; i < num; i += 1) {
+  //     if (num % i === 0) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // };
+
+  const isPrime2 = (num) => {
+    const iter = (n, i) => {
       if (num % i === 0) {
         return false;
       }
-    }
-    return true;
+      if (num % i !== 0) {
+        return true;
+      }
+
+      return iter(n, i + 1);
+    };
+
+    return iter(num, 2);
   };
 
-  const answer = isPrime(quest) === true ? 'yes' : 'no';
+  const answer = isPrime2(quest) === true ? 'yes' : 'no';
   return cons(quest, answer);
 };
 
