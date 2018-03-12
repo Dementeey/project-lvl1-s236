9ce8176350bf1ec331d23c2db5957a87cf13aea0
Dementeey-home <metallist9656@gmail.com>
2018-03-12 16:38:22 +0200

    fix problem progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,17 +2,23 @@ import { cons } from 'hexlet-pairs';
 import engine, { getRandomInt } from '..';
 
 const rules = 'What number is missing in this progression?';
-const generator = () => {
+
+const progression = () => {
   const random1 = getRandomInt(1, 10);
   let num = 1;
   const numbers = [];
 
   for (let i = 0; i < 9; i += 1) {
-    numbers.push(num += random1);
+    numbers.push((num += random1));
   }
+  return numbers;
+};
 
-  const answer = numbers.splice(4, 1, '..');
-  const quest = numbers.join(' ');
+
+const generator = () => {
+  const arr = progression();
+  const answer = arr.splice(4, 1, '..');
+  const quest = arr.join(' ');
 
   return cons(quest, answer);
 };
